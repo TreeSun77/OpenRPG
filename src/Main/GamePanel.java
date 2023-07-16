db3c2bcb2173f4ca62f76a5b1cb4c0d4bd532e74
TreeSun77 <TreeSun77@TreeSun77.ru>
2023-07-17 00:28:10 +0300

    animation

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,14 +1,17 @@
 package Main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GamePanel extends JPanel implements Runnable {
 	// SCREEN SETTINGS
 	final int originalTileSize = 16;
 	final int scale = 3;
 
-	final int tileSize = originalTileSize * scale; // 48x48
+	public final int tileSize = originalTileSize * scale; // 48x48
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; //768
@@ -18,13 +21,14 @@ public class GamePanel extends JPanel implements Runnable {
 
 	KeyHandler keyH = new KeyHandler();
 	Thread gameTread;
+	Player player = new Player(this, keyH);
 	//set default player position
 	int playerX = 100;
 	int playerY = 100;
 	int playerSpeed = 4;
 
 
-	public GamePanel() {
+	public GamePanel() throws IOException {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.BLACK);
 		this.setDoubleBuffered(true);
@@ -63,23 +67,16 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 
 	public void update() {
-		if (keyH.upPressed == true) {
-			playerY -= playerSpeed;
-		} else if (keyH.downPressed == true) {
-			playerY += playerSpeed;
-		} else if (keyH.leftPressed == true) {
-			playerX -= playerSpeed;
-		} else if (keyH.rightPressed == true) {
-			playerX += playerSpeed;
-		}
+	player.update();
 
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.white);
-		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		 Graphics2D g2 = (Graphics2D) g;
+		player. draw(g2);
+
+
 		g2.dispose();
 	}
 }
---
+++ src/entity/Player.java
@@ -0,0 +1,93 @@
+package entity;
+
+import Main.GamePanel;
+import Main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Player extends Entity {
+	GamePanel gp;
+	KeyHandler keyH;
+	// Image player
+	File p1 = new File("scr/res/player/u1.png");
+	File p2 = new File("scr/res/player/u2.png");
+	File p3 = new File("scr/res/player/u3.png");
+	File p4 = new File("scr/res/player/d1.png");
+	File p5 = new File("scr/res/player/d2.png");
+	File p6 = new File("scr/res/player/d3.png");
+	File p7 = new File("scr/res/player/l1.png");
+	File p8 = new File("scr/res/player/l2.png");
+	File p9 = new File("scr/res/player/l3.png");
+	File p10 = new File("scr/res/player/r1.png");
+	File p11 = new File("scr/res/player/r2.png");
+	File p12 = new File("scr/res/player/r3.png");
+
+	public Player(GamePanel gp, KeyHandler keyH) throws IOException {
+		this.gp = gp;
+		this.keyH = keyH;
+		setDefaultValues();
+	  	getPlayerImage();
+	}
+
+	public void setDefaultValues() {
+		x = 100;
+		y = 100;
+		speed = 4;
+		direction = "down";
+	}
+
+	public void getPlayerImage() throws IOException {
+		up1 = ImageIO.read(p1);
+		up2 = ImageIO.read(p2);
+		up3 = ImageIO.read(p3);
+		down1 = ImageIO.read(p4);
+		down2 = ImageIO.read(p5);
+		down3 = ImageIO.read(p6);
+		left1 = ImageIO.read(p7);
+		left2 = ImageIO.read(p8);
+		left3 = ImageIO.read(p9);
+		right1 = ImageIO.read(p10);
+		right2 = ImageIO.read(p11);
+		right3 = ImageIO.read(p12);
+	}
+
+	public void update() {
+		if (keyH.upPressed == true) {
+			direction ="up";
+			y -= speed;
+		} else if (keyH.downPressed == true) {
+			direction ="down";
+			y += speed;
+		} else if (keyH.leftPressed == true) {
+			direction ="left";
+			x -= speed;
+		} else if (keyH.rightPressed == true) {
+			direction ="right";
+			x += speed;
+		}
+	}
+
+	public void draw(Graphics2D g2) {
+		BufferedImage image = null;
+		switch (direction){
+			case "up":
+				image = up1;
+				break;
+			case "down":
+				image = down1;
+				break;
+			case "left":
+				image = left1;
+				break;
+			case "right":
+				image = right1;
+				break;
+		}
+		g2.drawImage(image, x,y, gp.tileSize, gp.tileSize, null);
+	}
+
+}
